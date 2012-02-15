3933231851c66463ce5ad52776915fc3520aa1a4
lawlesst <lawlesst@gmail.com>
2012-02-15 17:31:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-bul-link",
-    version = "1",
-    url = 'https://github.com/lawlesst/django-bul-link',
-    description = "An interface to the Serial Solutions 360 Link API.",
-    long_description = read('README'),
-
-    author = 'Ted Lawless',
-    author_email = 'lawlesst@gmail.com',
-
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    
-    install_requires = ['setuptools'],
-
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
+    version='0.1',
+    packages=find_packages(),
+    test_suite = 'py360link.test'
 )
