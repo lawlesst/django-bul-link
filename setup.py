9cd806639007ad13e93a52f9315d4df40b6dca33
lawlesst <lawlesst@gmail.com>
2012-02-16 12:30:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = "django-bul-link",
     version='1',
-    packages=find_packages(),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
 )
