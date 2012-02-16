ddd9d96e34ea63691454a9ed4285c49ba87bb040
lawlesst <lawlesst@gmail.com>
2012-02-16 12:26:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-bul-link",
-    version='0.1',
+    name = "bul_link",
+    version='1',
     packages=find_packages(),
-    test_suite = 'py360link.test'
 )
